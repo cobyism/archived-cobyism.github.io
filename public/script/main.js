e7d807935e43caecf7ba4df23d0a193093e23946
Coby Chapple <coby@cobychapple.com>
2016-02-18 13:14:32 +0000

    tweak some stuff to work with turbolinks

--- public/script/main.js
+++ public/script/main.js
@@ -1,20 +1,26 @@
 // A function for use with regular page loads AND turbolinks loads
-var ready = function() {
+var ready;
+ready = function() {
   // Calculate reading times
   $(".js-read-time").text(function (index, value) {
     return Math.ceil(parseFloat(value));
   });
 
   // Instafeed
-  var userFeed = new Instafeed({
-    get: 'user',
-    userId: 185695,
-    accessToken: '185695.0ba7f55.7d6189f0d88d4f4aab82f134388b032b',
-    resolution: 'low_resolution',
-    limit: '4'
-  });
-  userFeed.run();
+  instafeed = $("#instafeed")
+  if (instafeed.length && !instafeed.attr('populated')){
+    var userFeed = new Instafeed({
+      get: 'user',
+      userId: 185695,
+      accessToken: '185695.0ba7f55.7d6189f0d88d4f4aab82f134388b032b',
+      resolution: 'low_resolution',
+      limit: '4'
+    });
+    userFeed.run();
+    // Save that this has been run, for turblinks
+    instafeed.attr("populated", "true")
+  }
 }
 
-$(document).ready(ready);
-$(document).on('page:load', ready);
+$(document).on('ready', ready);
+$(document).on('turbolinks:load', ready);
